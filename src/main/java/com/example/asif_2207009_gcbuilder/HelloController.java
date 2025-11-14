e3ba39227ad8407ae99de5e60ce8155848c81119
Asif Istiaque <asifistiaq47@gmail.com>
2025-11-14 19:57:10 +0600

    Initial Commit

---
+++ src/main/java/com/example/asif_2207009_gcbuilder/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.asif_2207009_gcbuilder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.asif_2207009_gcbuilder {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.asif_2207009_gcbuilder to javafx.fxml;
+    exports com.example.asif_2207009_gcbuilder;
+}
